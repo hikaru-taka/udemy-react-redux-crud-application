913212606124094caac341e3a3d42bbebf4699a3
hikaru-taka <hikaru@couger.co.jp>
2018-12-18 16:23:05 +0900

    bug fix

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
-  </Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
